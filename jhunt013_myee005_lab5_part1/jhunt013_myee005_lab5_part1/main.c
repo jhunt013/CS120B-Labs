0b04e41097e147a81163a08f143804fbd447372c
Jennifer Hunter <jhunt013@ucr.edu>
2019-04-16 00:18:46 -0700

    lab5

---
+++ jhunt013_myee005_lab5_part1/jhunt013_myee005_lab5_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * jhunt013_myee005_lab5_part1.c
+ *
+ * Created: 4/16/2019 12:09:45 AM
+ * Author : jenny
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
