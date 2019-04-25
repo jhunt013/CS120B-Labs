72325315ffecd2db8c67bc43de3d809ca2808140
Jennifer Hunter <jhunt013@ucr.edu>
2019-04-24 23:21:28 -0700

    lab 7 file creation

---
+++ jhunt013_myee005_lab7_part1/jhunt013_myee005_lab7_part1/main.c
@@ -0,0 +1,22 @@
+/*
+ * jhunt013_myee005_lab7_part1.c
+ *
+ * Created: 4/24/2019 11:20:17 PM
+*  Partner 1 Name & E-mail: Jennifer Hunter jhunt013@ucr.edu
+*  Partner 2 Name & E-mail: Marcus Yee myee005@ucr.edu
+*  Lab Section: 028
+*  Assignment: Lab #7  Exercise # 1
+*  I acknowledge all content contained herein, excluding template or example code, is my own original work.
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
